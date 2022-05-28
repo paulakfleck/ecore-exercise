472f2b6ab75f1156080851b47819f594a9787ddb
Paula Fleck <paulakfleck@gmail.com>
2022-05-28 12:24:11 -0300

    Create mock pages

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const  Welcome = () => {
+    return (
+        <React.Fragment>
+            <h1>Welcome to this e-Core exercise!</h1>
+            <h2>Here you can access teams, users, and users inside the teams.</h2>
+        </React.Fragment>
+    )
+}
+
+export default Welcome;
