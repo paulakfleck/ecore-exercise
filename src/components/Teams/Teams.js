435d389c27b31d9a71917f5ab4d1b66f4dbb1956
Paula Fleck <paulakfleck@gmail.com>
2022-05-30 10:48:47 -0300

    Change Teams to use useFetch

--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -1,43 +1,18 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React from 'react';
 
 import TeamsList from './TeamsList';
 
+import useFetch from './../../hooks/useFetch';
+
 const  Teams = () => {
-    const [teams, setTeams] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-
-    const fetchTeamsHandler = useCallback(async () => {
-        setIsLoading(true);
-        setError(null);
-
-        try {
-            const response = await fetch('https://cgjresszgg.execute-api.eu-west-1.amazonaws.com/teams/');
-
-            if (!response.ok) {
-                throw new Error('Something went wrong');
-            }
-            const data = await response.json();
-
-            setTeams(data);
-
-        } catch (error) {
-            setError(error.message);
-        }
-
-        setIsLoading(false);
-    }, []);
-
-    useEffect(() => {
-        fetchTeamsHandler();
-    }, [fetchTeamsHandler]);
+    const { data, error, isLoading } = useFetch('https://cgjresszgg.execute-api.eu-west-1.amazonaws.com/teams/');
 
     return (
         <React.Fragment>
             <h1>Teams</h1>
             <h2>List of all teams available</h2>
 
-            {!isLoading && <TeamsList teams={ teams }></TeamsList>}
+            {!isLoading && <TeamsList teams={ data }></TeamsList>}
 			{isLoading && <p>Loading...</p>}
 			{!isLoading && error && <p>{ error }</p>}
         </React.Fragment>
