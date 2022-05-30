bb83781861d54b910025c40349cb80c4b61aa728
Paula Fleck <paulakfleck@gmail.com>
2022-05-30 18:01:55 -0300

    Start unit tests

---
+++ src/components/Welcome.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+// import { createRoot } from 'react-dom/client';
+import Welcome from './Welcome';
+
+describe('Welcome component', () => {
+    test('renders "Welcome" correctly', () => {
+        render(<Welcome />);
+
+        const el = screen.getByText('Here you can access teams, team members, and members inside the teams.', {èxact: true});
+
+        expect(el).toBeInTheDocument();
+    })
+})
