2106bf1268fa40f36f5b97b25c9f49666e96ac4f
Paula Fleck <paulakfleck@gmail.com>
2022-05-30 16:00:39 -0300

    Create Search filter and style, and add it to Teams

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+const Search = (props) => {
+    const filterItems = (searchValue) => {
+        props.sendFilter(searchValue);
+    }
+
+    return (
+        <input className="search" placeholder={props.placeholder} onChange={(e) => filterItems(e.target.value)} />
+    )
+}
+
+export default Search;
--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -8,26 +8,42 @@ import useFetch from './../../hooks/useFetch';
 const Teams = () => {
     const url = 'https://cgjresszgg.execute-api.eu-west-1.amazonaws.com';
     const [teams, setTeams] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
+
+    const getFilter = (searchValue) => {
+        setSearchInput(searchValue);
+    }
 
     const defineTeams = teams => {
-        setTeams(teams);
+        const filteredTeams = teams.filter((team) => {
+            if (team.name.toLowerCase().includes(searchInput.toLowerCase())) {
+                return team;
+            }
+        });
+
+        setTeams(filteredTeams);
     }
 
     const { isLoading, error, fetchData: fetchTeams } = useFetch(`${url}/teams`, defineTeams);
 
-   useEffect(() => {
-    fetchTeams();
-   }, []);
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            console.log('debouncing')
+            fetchTeams();
+          }, 200)
+      
+          return () => clearTimeout(delayDebounceFn)
+    }, [searchInput]);
 
     return (
         <React.Fragment>
             <h1 className="yellow">Teams</h1>
 
-            <Search />
-
-            {!isLoading && <TeamsList teams={ teams }></TeamsList>}
-			{isLoading && <p className="loading">Loading...</p>}
-			{!isLoading && error && <p>{ error }</p>}
+            <Search sendFilter={getFilter} placeholder="Filter by Team name" />
+            
+            {!isLoading && <TeamsList teams={teams}></TeamsList>}
+            {isLoading && <p className="loading">Loading...</p>}
+            {!isLoading && error && <p>{error}</p>}
         </React.Fragment>
     )
 }
