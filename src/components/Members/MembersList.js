72030f3fcc2ffbe8f74353ee819ec41a081e8c69
Paula Fleck <paulakfleck@gmail.com>
2022-05-30 16:49:00 -0300

    Styling

--- src/components/Members/MembersList.js
+++ src/components/Members/MembersList.js
@@ -1,30 +1,34 @@
 import React from 'react';
 
 const MembersList = (props) => {
-    console.log( props.members);
+    if (typeof props.members !== 'boolean') {
+        // Since the images were not loading from cnd.fakercloud, I removed the "alt" text so the page is more friendly, but I prefer to add "alt" to images.
+        const listItems = props.members.map((member) =>
+            <li key={member.id}>
+                <figure>
+                    <img src={member.avatarUrl} />
+                    <figcaption>
+                        {member.firstName} {member.lastName}
+                    </figcaption>
+                </figure>
 
-    // Since the images were not loading from cnd.fakercloud, I removed the "alt" text so the page is more friendly, but I prefer to add "alt" to images.
-    const listItems = props.members.map((member) =>
-        <li key={member.id}>
-            <figure>
-                <img src={member.avatarUrl} />
-                <figcaption>
-                    {member.firstName} {member.lastName}
-                </figcaption>
-            </figure>
+                <h4 className="location">Based in: <span>{member.location}</span></h4>
+            </li>
+        );
 
-            <h4 className="location">Based in: <span>{member.location}</span></h4>
-        </li>
-    );
+        if (listItems.length === 0) {
+            return (
+                <ul className="empty-results">
+                    <li>No members found.</li>
+                </ul>
+            );
 
-    if (listItems.length === 0) {
-		return (<li>No members found.</li>);
-
-	} else {
-		return (
-			<ul className='list-of-cards'>{listItems}</ul>
-		)
-	}
+        } else {
+            return (
+                <ul className="list-of-cards">{listItems}</ul>
+            )
+        }
+    }
 }
 
 export default MembersList;
