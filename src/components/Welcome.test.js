a4e541d245b7c1fe1fd60f05c7abf5390f44c087
Paula Fleck <paulakfleck@gmail.com>
2022-05-31 09:43:45 -0300

    Update Welcome.test.js

--- src/components/Welcome.test.js
+++ src/components/Welcome.test.js
@@ -6,7 +6,7 @@ describe('Welcome component', () => {
     test('renders "Welcome" correctly', () => {
         render(<Welcome />);
 
-        const el = screen.getByText('Here you can access teams, team members, and members inside the teams.', {èxact: true});
+        const el = screen.getByText('Here you can access teams, team members, and members inside the teams.', {exact: true});
 
         expect(el).toBeInTheDocument();
     })
